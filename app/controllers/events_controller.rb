2a07a248a91c1ce90fc8385af20af88fc4a49a95
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-30 10:27:03 +0200

    update event model-

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,51 +1,54 @@
 class EventsController < ApplicationController
 
-class BabyBottle < EventsController
-  
-end
 
-class Meal < EventsController
-  
-end
 
-class Sleep < EventsController
-  
-end
 
-class Diaper < EventsController
-  
-end
+  class BabyBottle < EventsController
+    
+  end
 
-class Wound < EventsController
-  
-end
+  class Meal < EventsController
+    
+  end
 
-class Vaccine < EventsController
-  
-end
+  class Sleep < EventsController
+    
+  end
 
-class Temperature < EventsController
-  
-end
+  class Diaper < EventsController
+    
+  end
 
-class Drug < EventsController
-  
-end
+  class Wound < EventsController
+    
+  end
 
-class Disease < EventsController
-  
-end
+  class Vaccine < EventsController
+    
+  end
 
-class Weight < EventsController
-  
-end
+  class Temperature < EventsController
+    
+  end
 
-class Height < EventsController
-  
-end
+  class Drug < EventsController
+    
+  end
 
-class OtherEvent < EventsController
-  
-end
+  class Disease < EventsController
+    
+  end
+
+  class Weight < EventsController
+    
+  end
+
+  class Height < EventsController
+    
+  end
+
+  class OtherEvent < EventsController
+    
+  end
 
 end
