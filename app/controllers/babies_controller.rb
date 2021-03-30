81fcf36394b707edfdcf11134da29cd37f622878
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-30 11:07:12 +0200

    added strong params method

--- app/controllers/babies_controller.rb
+++ app/controllers/babies_controller.rb
@@ -2,4 +2,10 @@ class BabiesController < ApplicationController
   def index
     @babies = Baby.all
   end
+
+  private
+
+  def article_params
+  params.require(:baby).permit(:name, :birth_date, :weight, :height, :gender, :photo)
+end
 end
