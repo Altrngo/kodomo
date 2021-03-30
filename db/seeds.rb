af5ba02329a9d711295793ff9fa7c1585b120c3b
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-03-30 16:19:35 +0200

    adding last merges

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
+require "open-uri"
+
 puts 'Cleaning database...'
 Baby.destroy_all
+
 puts 'Creating Babies...'
+file = URI.open('https://res.cloudinary.com/dw4harw6z/image/upload/v1617095840/Baby_1_umrtjv.jpg')
 baby_1 = Baby.create(name:"Mario",birth_date: 20210328, weight: 2.4, height: 53, gender: "female")
+baby_1.photo.attach(io: file, filename: 'baby_1.jpg', content_type: 'image/jpg')
 puts "Created Baby 1 "
+
+file = URI.open('https://res.cloudinary.com/dw4harw6z/image/upload/v1617095801/Baby_2_dkgv0v.jpg')
 baby_2 = Baby.create(name:"Maria",birth_date: 20210327, weight: 3.7, height: 56, gender: "male")
+baby_2.photo.attach(io: file, filename: 'baby_2.jpg', content_type: 'image/jpg')
 puts "Created Baby 2 "
+
+file = URI.open('https://res.cloudinary.com/dw4harw6z/image/upload/v1617095849/Baby_3_qrer8g.jpg')
 baby_3 = Baby.create(name:"Leo",birth_date: 20210227, weight: 3.0, height: 58, gender: "male")
+baby_3.photo.attach(io: file, filename: 'baby_3.jpg', content_type: 'image/jpg')
 puts "Created Baby 3 "
+
+file = URI.open('https://res.cloudinary.com/dw4harw6z/image/upload/v1617108891/Baby_4_ugkjls.jpg')
+baby_4 = Baby.create(name:"Constantin",birth_date: 20210127, weight: 2.0, height: 48, gender: "male")
+baby_4.photo.attach(io: file, filename: 'baby_4.jpg', content_type: 'image/jpg')
+puts "Created Baby 4 "
+
 puts 'Finished!'
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
