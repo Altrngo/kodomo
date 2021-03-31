15294ce922e95d0114b8af61050aa8656bd1facb
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-03-31 10:32:36 +0200

    added validation in baby table

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -2,4 +2,6 @@ class Baby < ApplicationRecord
 has_one_attached :photo
 has_many :events, dependent: :destroy
 has_many :users, :through => :baby_users
+validates :name, presence: true
+validates :birth_date, :gender, :weight, :height, presence: true
 end
