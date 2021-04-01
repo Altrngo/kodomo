b7650d269169087be0c0ebe9fa64777cc2b5c0ed
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-04-01 18:04:01 +0200

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 require "open-uri"
 
+# puts 'Cleaning database...'
+# Event.destroy_all
+# puts 'Creating Bibi...'
+# bibi = Event.create!(start_time: DateTime.new(2021,3,6,12,46,0), end_time: DateTime.new(2021,3,5,12,47,0), type: "Biberon", value_float: 250, comment:"a fait son rot", validated: true, baby_id: 30, user_id: 1)
+# bibi = Event.create!(start_time: DateTime.new(2021,3,7,12,47,0), end_time: DateTime.new(2021,3,5,12,48,0), type: "Repas", value_float: 200, comment:"a rien bu", validated: true, baby_id: 23, user_id: 1)
+# bibi = Event.create!(start_time: DateTime.new(2021,3,8,12,48,0), end_time: DateTime.new(2021,3,5,12,49,0), type: "Dodo", value_float: 300, comment:"a trop bu", validated: true, baby_id: 30, user_id: 1)
+# bibi = Event.create!(start_time: DateTime.new(2021,3,9,12,49,0), end_time: DateTime.new(2021,3,5,12,50,0), type: "Couche", value_float: 400, comment:"aurait pu boire plus", validated: true, baby_id: 23, user_id: 1)
+# bibi = Event.create!(start_time: DateTime.new(2021,3,10,12,50,0), end_time: DateTime.new(2021,3,5,12,51,0), type: "Biberon", value_float: 500, comment:"a vomi", validated: true, baby_id: 30, user_id: 1)
+# bibi = Event.create!(start_time: DateTime.new(2021,3,9,12,49,0), end_time: DateTime.new(2021,3,5,12,54,0), type: "Repas", value_float: 600, comment:"aurait pu boire plus bis", validated: true, baby_id: 23, user_id: 1)
+# puts "Bibi Created... "
+# puts 'Finished!'
+
+
 puts 'Cleaning database...'
 Event.destroy_all
 puts 'Creating Bibi...'
