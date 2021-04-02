3756ff2cc8095a6f58e54e48f70f21a23d0bd3f6
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-04-02 10:38:47 +0200

    added method to select events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,7 +13,8 @@ class EventsController < ApplicationController
 
   def timeline
     @baby = Baby.find(params[:id])
-    @events = Event.where(baby: @baby).sort_by { |event| event.start_time}.reverse
+    @events = Event.where(baby: @baby).sort_by { |event| event.start_time}.reverse || Event.where("start_time ILIKE ?", "%#{params[:start_time]}%")
+    @events = Event.where("start_time ILIKE ?", "%#{params[:start_time]}%")
   end
 
   def new
