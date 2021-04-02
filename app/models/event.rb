1c6d607c4b58e9e2fc3fa6028c228e762629dac7
Ophelie Bugnon <ophelie.bugnon@gmail.com>
2021-04-02 10:32:54 +0200

    added gem pgsearch + insert form in view + adapt model event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
   DIAPER_CONTENT = %w(Urine Souillée Mixte)
   belongs_to :baby
   belongs_to :user
+  include PgSearch::Model
+  pg_search_scope :search_by_start_time,
+    against: [ :start_time ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
