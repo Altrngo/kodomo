ef2bca4a8c7a25644de0715f008491d349a8a7fd
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-04-05 17:20:52 +0200

    event show update

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,23 @@ class Event < ApplicationRecord
   DIAPER_CONTENT = %w(Urine Selles Mixte)
   belongs_to :baby
   belongs_to :user
+
+  def calculate_duration
+    duration = (self.end_time - self.start_time)
+
+    durationLabel = []
+    separator     = " "
+
+    hours = (duration / 3600).floor
+    restDuration = duration%3600
+    durationLabel << "#{hours}h" if hours > 0
+    return durationLabel.join separator if restDuration.floor == 0
+    minutes = (restDuration / 60).floor
+    restDuration = duration%60
+    durationLabel << "#{minutes}m" if minutes > 0
+    return durationLabel.join separator if restDuration.floor == 0
+    seconds = restDuration.floor
+    durationLabel << "#{seconds}s" if seconds > 0
+    durationLabel.join separator
+    end
 end
